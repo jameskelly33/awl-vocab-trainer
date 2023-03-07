7cd98b4638c3012b2bcf007f491576fde77c894b
James Kelly <jameskelly33@gmail.com>
2023-03-07 10:29:25 +0000

    basic random word build complete

--- script.js
+++ script.js
@@ -30,16 +30,20 @@ soundLink.addEventListener('click', (event) => {
 
 
 function openTab(evt, tabName) {
+  
     var i, tabcontent, tablinks;
     tabcontent = document.getElementsByClassName("tabcontent");
     for (i = 0; i < tabcontent.length; i++) {
       tabcontent[i].classList.remove("active");
+     
     }
     tablinks = document.getElementsByClassName("tablinks");
     for (i = 0; i < tablinks.length; i++) {
       tablinks[i].classList.remove("active");
+   
+      
     }
     document.getElementById(tabName).classList.add("active");
     evt.currentTarget.classList.add("active");
+    
   }
-  
